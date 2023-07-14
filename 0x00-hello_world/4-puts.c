2fa7f14b32079f958982a3413936e6b567eadb3b
Maro Ridhiwani <maroridhiwani@gmail.com>
2023-07-14 05:24:20 +0300

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - This is a program that utilizes puts to print a line
+* Return: 0 (The program will return zero when it has successfully run)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - a program that utilizes printf to print a line
+* Return:0 (upon successful execution)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
