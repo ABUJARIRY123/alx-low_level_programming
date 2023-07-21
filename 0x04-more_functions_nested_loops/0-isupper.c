e5f30c0f6da5b196f658a5346aa3152e37450d96
Maro Ridhiwani <maroridhiwani@gmail.com>
2023-07-21 05:23:42 +0300

    number one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - This program checks if a letter is upper
+ *@l: the letter to be checked
+ * Return: 1 for uppercase, 0 else
+*/
+int _isupper(int l)
+{
+if (l >= 65 && l <= 90)
+{
+return (1);
+}
+return (0);
+}
